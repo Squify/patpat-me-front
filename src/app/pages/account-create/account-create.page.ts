ce313299b5a5f644791ba95fc735b3d7a7e917b1
Pauline <40692004+Squify@users.noreply.github.com>
2020-04-01 19:31:37 +0200

    [PAT-2] gestion des erreurs

--- src/app/pages/account-create/account-create.page.ts
+++ src/app/pages/account-create/account-create.page.ts
@@ -5,6 +5,7 @@ import {UserService} from '../../services/user/user.service';
 import {HttpErrorResponse} from '@angular/common/http';
 import {UserGender} from '../../interfaces/user-gender';
 import {GenderService} from '../../services/gender/gender.service';
+import {ToastController} from '@ionic/angular';
 
 @Component({
     selector: 'app-account-create',
@@ -18,7 +19,11 @@ export class AccountCreatePage implements OnInit {
     genders: UserGender[] = [];
 
     // Errors
+    unknownError: boolean;
+    serverError: boolean;
+    inputsError: boolean;
     mailInputError: boolean;
+    mailAlreadyUsed: boolean;
     passwordInputError: boolean;
     passwordSecurityError: boolean;
     pseudoInputError: boolean;
@@ -29,6 +34,7 @@ export class AccountCreatePage implements OnInit {
     constructor(
         private userService: UserService,
         private genderService: GenderService,
+        public toastController: ToastController
     ) {
 
         this.buildForm();
@@ -108,7 +114,11 @@ export class AccountCreatePage implements OnInit {
     }
 
     setAllErrorsToFalse(): void {
+        this.serverError = false;
+        this.unknownError = false;
+        this.inputsError = false;
         this.mailInputError = false;
+        this.mailAlreadyUsed = false;
         this.passwordInputError = false;
         this.passwordSecurityError = false;
         this.pseudoInputError = false;
@@ -122,6 +132,7 @@ export class AccountCreatePage implements OnInit {
             this.createAccount();
             this.connectUser();
         } else {
+            this.presentToast('general');
             console.log('ça marche po');
         }
     }
@@ -167,16 +178,16 @@ export class AccountCreatePage implements OnInit {
             this.passwordInputError = true;
         }
         if (this.createPersonForm.controls.pseudo.errors) {
-            this.mailInputError = true;
+            this.pseudoInputError = true;
         }
         if (this.createPersonForm.controls.lastname.errors) {
-            this.passwordInputError = true;
+            this.lastnameInputError = true;
         }
         if (this.createPersonForm.controls.firstname.errors) {
-            this.mailInputError = true;
+            this.firstnameInputError = true;
         }
         if (this.createPersonForm.controls.phone.errors) {
-            this.passwordInputError = true;
+            this.phoneInputError = true;
         }
     }
 
@@ -201,17 +212,111 @@ export class AccountCreatePage implements OnInit {
         if (error) {
             switch (error.status) {
                 case 400:
-                    // this.inputsError = true;
+                    this.inputsError = true;
+                    this.presentToast('back_input');
+                    break;
+                case 409:
+                    this.inputsError = true;
+                    this.presentToast('back_pseudo_used');
+                    break;
+                case 417:
+                    this.mailAlreadyUsed = true;
+                    this.presentToast('back_mail_used');
                     break;
                 case 500:
-                    // this.serverError = true;
+                    this.serverError = true;
+                    this.presentToast('back_server');
                     break;
                 default:
-                // this.unknownError = true;
+                    this.unknownError = true;
+                    this.presentToast('back_unknown');
+                    break;
             }
         } else {
-            // this.unknownError = true;
+            this.unknownError = true;
+            this.presentToast('back_unknown');
         }
+    }
+
+    async presentToast(error: string) {
+        let toast: HTMLIonToastElement;
+        switch (error) {
+            case 'pseudo':
+                toast = await this.toastController.create({
+                    message: 'Veuillez renseigner un pseudo valide.',
+                    duration: 2000
+                });
+                break;
+            case 'password':
+                toast = await this.toastController.create({
+                    message: 'Veuillez renseigner un mot de passe valide et sécurisé.',
+                    duration: 2000
+                });
+                break;
+            case 'mail':
+                toast = await this.toastController.create({
+                    message: 'Veuillez renseigner une adresse email valide.',
+                    duration: 2000
+                });
+                break;
+            case 'lastname':
+                toast = await this.toastController.create({
+                    message: 'Veuillez renseigner un nom valide.',
+                    duration: 2000
+                });
+                break;
+            case 'firstname':
+                toast = await this.toastController.create({
+                    message: 'Veuillez renseigner un prénom valide.',
+                    duration: 2000
+                });
+                break;
+            case 'phone':
+                toast = await this.toastController.create({
+                    message: 'Veuillez renseigner un numéro de téléphone valide.',
+                    duration: 2000
+                });
+                break;
+            case 'back_input':
+                toast = await this.toastController.create({
+                    message: 'La requête est invalide, vérifiez les informations saisies.',
+                    duration: 2000
+                });
+                break;
+            case 'back_pseudo_used':
+                toast = await this.toastController.create({
+                    message: 'Le pseudo saisi est déjà utilisé.',
+                    duration: 2000
+                });
+                break;
+            case 'back_mail_used':
+                toast = await this.toastController.create({
+                    message: 'L\'adresse email saisie est déjà utilisée.',
+                    duration: 2000
+                });
+                break;
+            case 'back_server':
+                toast = await this.toastController.create({
+                    message: 'Une erreur serveur est survenue.',
+                    duration: 2000
+                });
+                break;
+            case 'back_unknown':
+                toast = await this.toastController.create({
+                    message: 'Une erreur inconnue est survenue.',
+                    duration: 2000
+                });
+                break;
+            default:
+                toast = await this.toastController.create({
+                    message: 'Le formulaire est erroné ! Cliquez sur les ronds pour plus d\'infos.',
+                    duration: 3000
+                });
+                break;
+
+        }
+
+        toast.present();
     }
 
 }
