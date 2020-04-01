3fd880b50c4d237e7528fb9583d78b85331df7cd
Pauline <gouleypauline@live.fr>
2020-04-01 12:13:24 +0200

    [PAT-2] create account without gender choice and without style

---
+++ src/app/interfaces/user-gender.ts
@@ -0,0 +1,4 @@
+
+export interface UserGender {
+    nom: string;
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {CreateAccount} from '../interfaces/create-account';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+
+    constructor(
+        private http: HttpClient,
+    ) {
+    }
+
+    createUser(createAccount: CreateAccount): Observable<any> {
+      return this.http.post<any>(environment.BACKEND_URL + '/api/user/create', createAccount);
+    }
+}
