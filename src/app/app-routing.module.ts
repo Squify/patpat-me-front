4505db86a614603fa322f76939761cf61bee7a3c
Pauline <40692004+Squify@users.noreply.github.com>
2020-04-03 16:15:05 +0200

    [PAT-6] création d'un event avec date & type + récupération des types

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ const routes: Routes = [
   {
     path: 'account-create',
     loadChildren: () => import('./pages/account-create/account-create.module').then( m => m.AccountCreatePageModule)
+  },
+  {
+    path: 'event-create',
+    loadChildren: () => import('./pages/event-create/event-create.module').then( m => m.EventCreatePageModule)
   }
 ];
 @NgModule({
