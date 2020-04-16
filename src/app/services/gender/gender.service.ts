f0f3a40031a73e78a71d4f06c1f8877e8eaa662f
Lucie <lucie.calem@gmail.com>
2020-04-16 18:42:01 +0200

    ajout des services animal/type et add AnimalGender

--- src/app/services/gender/gender.service.ts
+++ src/app/services/gender/gender.service.ts
@@ -4,16 +4,22 @@ import {Observable} from 'rxjs';
 import {UserGender} from '../../interfaces/user-gender';
 import {environment} from '../../../environments/environment';
 import {log} from 'util';
+import { AnimalGender } from 'src/app/interfaces/animal-gender';
 
 @Injectable({
     providedIn: 'root'
 })
 export class GenderService {
     genders: UserGender[];
+    animalGenders: AnimalGender[];
 
     constructor(
         private http: HttpClient
     ) {
+    }
+
+    getAnimalGender(): Observable<AnimalGender[]> {
+        return this.http.get<AnimalGender[]>(environment.BACKEND_URL + '/api/animal/genders');
     }
 
     getUserGender(): Observable<UserGender[]> {
