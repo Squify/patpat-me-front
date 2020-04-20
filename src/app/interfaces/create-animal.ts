a39eae5290b5b97fc215bc86cd70c4dd995e152f
Lucie <lucie.calem@gmail.com>
2020-04-20 15:26:42 +0200

    add race and temper

--- src/app/interfaces/create-animal.ts
+++ src/app/interfaces/create-animal.ts
@@ -1,6 +1,7 @@
 export interface CreateAnimal {
     name: string;
     birthday: string;
+    fk_id_temper: string;
     fk_id_gender: string;
     fk_id_type: string;
     fk_id_race: string;
--- src/app/services/animal/animal.service.ts
+++ src/app/services/animal/animal.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { AnimalRace } from 'src/app/interfaces/animal-race';
+import { AnimalTemper } from 'src/app/interfaces/animal-temper';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimalService {
 
-  constructor() { }
+  races: AnimalRace[];
+  tempers: AnimalTemper[];
+
+  constructor(
+      private http: HttpClient
+  ) {
+  }
+
+  getAnimalTemper(): Observable<AnimalTemper[]> {
+    return this.http.get<AnimalTemper[]>(environment.BACKEND_URL + '/api/animal/tempers');
+}
+  getAnimalRace(): Observable<AnimalRace[]> {
+      return this.http.get<AnimalRace[]>(environment.BACKEND_URL + '/api/animal/races');
+  }
 }
