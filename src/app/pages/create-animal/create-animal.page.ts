496142cfdd99975e1da762ac166f252bf53ea22b
Lucie <lucie.calem@gmail.com>
2020-04-20 16:41:26 +0200

    add errors, race error

--- src/app/pages/create-animal/create-animal.page.ts
+++ src/app/pages/create-animal/create-animal.page.ts
@@ -8,6 +8,8 @@ import { TypeService } from 'src/app/services/type/type.service';
 import { AnimalService } from 'src/app/services/animal/animal.service';
 import { AnimalTemper } from 'src/app/interfaces/animal-temper';
 import { AnimalRace } from 'src/app/interfaces/animal-race';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-animal',
@@ -23,10 +25,22 @@ export class CreateAnimalPage implements OnInit {
   races: AnimalRace[] = [];
   tempers: AnimalTemper[] = [];
 
+  // Errors
+  unknownError: boolean;
+  serverError: boolean;
+  inputsError: boolean;
+  nameInputError: boolean;
+  birthdayError: boolean;
+  typeError: boolean;
+  raceError: boolean;
+  temperError: boolean;
+  genderError: boolean;
+
   constructor(
     private genderService: GenderService,
     private typeService: TypeService,
     private animalService: AnimalService,
+    public toastController: ToastController,
 
   ) {
 
@@ -117,8 +131,25 @@ export class CreateAnimalPage implements OnInit {
     );
   }
 
+  setAllErrorsToFalse(): void {
+    this.serverError = false;
+    this.unknownError = false;
+    this.inputsError = false;
+    this.nameInputError = false;
+    this.birthdayError = false;
+    this.raceError = false;
+    this.temperError = false;
+    this.genderError = false;
+    this.typeError = false;
+  }
+
   submit(): void {
-    this.createAnimal();
+    if (this.formIsValid()) {
+      this.createAnimal();
+    } else {
+      this.presentToast('general');
+      console.log('ça marche po');
+    }
   }
 
   createAnimal(): void {
@@ -133,7 +164,124 @@ export class CreateAnimalPage implements OnInit {
 
     this.animalService.createAnimal(this.createAnimalInterface).subscribe(
       _ => console.log("animal creer"),
-      error => console.log("sa marche po"))
-    ;
+      error => this.processError(error));
   }
+
+  formIsValid(): boolean {
+    this.setAllErrorsToFalse();
+    this.checkInputsError();
+
+    return !this.createAnimalForm.invalid;
+  }
+
+  checkInputsError(): void {
+    if (this.createAnimalForm.controls.name.errors) {
+      this.nameInputError = true;
+    }
+    if (this.createAnimalForm.controls.birthday.errors) {
+      this.birthdayError = true;
+    }
+    if (this.createAnimalForm.controls.fk_id_type.errors) {
+      this.typeError = true;
+    }
+    if (this.createAnimalForm.controls.fk_id_gender.errors) {
+      this.genderError = true;
+    }
+    if (this.createAnimalForm.controls.fk_id_temper.errors) {
+      this.temperError = true;
+    }
+    if (this.createAnimalForm.controls.fk_id_race.errors) {
+      this.raceError = true;
+    }
+  }
+
+  processError(error: HttpErrorResponse) {
+    if (error) {
+      switch (error.status) {
+        case 400:
+          this.inputsError = true;
+          this.presentToast('back_input');
+          break;
+        case 500:
+          this.serverError = true;
+          this.presentToast('back_server');
+          break;
+        default:
+          this.unknownError = true;
+          this.presentToast('back_unknown');
+          break;
+      }
+    } else {
+      this.unknownError = true;
+      this.presentToast('back_unknown');
+    }
+  }
+
+  async presentToast(error: string) {
+    let toast: HTMLIonToastElement;
+    switch (error) {
+      case 'name':
+        toast = await this.toastController.create({
+          message: 'Veuillez renseigner un nom valide.',
+          duration: 2000
+        });
+        break;
+      case 'birthday':
+        toast = await this.toastController.create({
+          message: 'Veuillez renseigner une date d\'anniversaire.',
+          duration: 2000
+        });
+        break;
+      case 'fk_id_type':
+        toast = await this.toastController.create({
+          message: 'Veuillez renseigner le type de l\'animal.',
+          duration: 2000
+        });
+        break;
+      case 'fk_id_genre':
+        toast = await this.toastController.create({
+          message: 'Veuillez renseigner le genre de l\'animal.',
+          duration: 2000
+        });
+        break;
+      case 'fk_id_race':
+        toast = await this.toastController.create({
+          message: 'Veuillez renseigner la racede l\'animal.',
+          duration: 2000
+        });
+        break;
+      case 'fk_id_temper':
+        toast = await this.toastController.create({
+          message: 'Veuillez renseigner le temperament de l\'animal.',
+          duration: 2000
+        });
+        break;
+      case 'back_input':
+        toast = await this.toastController.create({
+          message: 'La requête est invalide, vérifiez les informations saisies.',
+          duration: 2000
+        });
+        break;
+      case 'back_server':
+        toast = await this.toastController.create({
+          message: 'Une erreur serveur est survenue.',
+          duration: 2000
+        });
+        break;
+      case 'back_unknown':
+        toast = await this.toastController.create({
+          message: 'Une erreur inconnue est survenue.',
+          duration: 2000
+        });
+        break;
+      default:
+        toast = await this.toastController.create({
+          message: 'Le formulaire est erroné ! Cliquez sur les ronds pour plus d\'infos.',
+          duration: 3000
+        });
+        break;
+    }
+    toast.present();
+  }
+
 }
