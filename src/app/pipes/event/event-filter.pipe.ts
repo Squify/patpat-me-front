cbb5fc17efd91a6f8992cee84891e0aee1c90f79
Pauline <40692004+Squify@users.noreply.github.com>
2020-05-04 22:26:58 +0200

    re-add Veniane work - events page

---
+++ src/app/pipes/event/event-filter.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'eventFilter'
+})
+export class EventFilterPipe implements PipeTransform {
+
+  transform(events: any[], filter: any): any {
+    if (!events || !filter) {
+      return events;
+    }
+    if (filter.length == 0) {
+      return events;
+    }
+    let result = [];
+    events.forEach(item => {
+      if (filter.includes(item.type)) {
+        result.push(item)
+      }
+    })
+    return result;
+  }
+
+}
