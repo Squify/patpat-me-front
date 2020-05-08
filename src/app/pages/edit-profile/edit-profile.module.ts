fb4e6c4332422ea934172de6d6352c4f215bcc34
Jonathan <jonathan@ileotech.com>
2020-05-08 13:16:13 +0200

    Edit profile page and form

---
+++ src/app/pages/edit-profile/edit-profile.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { EditProfilePageRoutingModule } from './edit-profile-routing.module';
+
+import { EditProfilePage } from './edit-profile.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    EditProfilePageRoutingModule,
+    ReactiveFormsModule
+  ],
+  declarations: [EditProfilePage]
+})
+export class EditProfilePageModule { }
