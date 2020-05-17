241960a43401a2d1aeb33ec01760b691029750a0
Pauline <40692004+Squify@users.noreply.github.com>
2020-05-18 00:33:13 +0200

    user can add or remove partipation to event
    missing error management

--- src/app/pages/event/event.page.ts
+++ src/app/pages/event/event.page.ts
@@ -1,82 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { EventService } from '../../services/event/event.service';
-import { EventInterface } from '../../interfaces/event/event-interface';
-import { EventType } from '../../interfaces/event/event-type';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {EventService} from '../../services/event/event.service';
+import {EventInterface} from '../../interfaces/event/event-interface';
+import {EventType} from '../../interfaces/event/event-type';
 
 @Component({
-  selector: 'app-event',
-  templateUrl: './event.page.html',
-  styleUrls: ['./event.page.scss'],
+    selector: 'app-event',
+    templateUrl: './event.page.html',
+    styleUrls: ['./event.page.scss'],
 })
 export class EventPage implements OnInit {
 
-  event: EventInterface;
-  types: EventType[] = [];
+    event: EventInterface;
+    types: EventType[] = [];
 
-  selectedSegment = 'informationSegment';
-  type = 'Randonnée';
-  url = '/src/assets/images/event-type/' + this.type + '.jpg';
-  private img = '/assets/images/grumpy.jpg';
-  private participants = [
-    {
-      avatar: '',
-      pseudo: 'PetitPoney',
-      role: ''
-    },
-    {
-      avatar: '',
-      pseudo: 'PetitChat',
-      role: ''
-    },
-    {
-      avatar: '',
-      pseudo: 'PetitChien',
-      role: ''
-    },
-    {
-      avatar: '',
-      pseudo: 'Chocolat',
-      role: ''
+    selectedSegment = 'informationSegment';
+    type = 'Randonnée';
+    url = '/src/assets/images/event-type/' + this.type + '.jpg';
+    private img = '/assets/images/grumpy.jpg';
+
+    constructor(
+        private eventService: EventService,
+        private ngZone: NgZone
+    ) {
     }
-  ];
 
-  constructor(
-      private eventService: EventService,
-  ) {
-  }
+    ngOnInit() {
+        this.getTypes();
+        this.getEvent();
+    }
 
-  ngOnInit() {
-    this.getTypes();
-    this.getEvent();
-  }
+    getEvent(): void {
 
-  getEvent(): void {
+        this.eventService.getEventById(1).subscribe(
+            value => {
+                this.event = value;
+            },
+            e => console.log(e)
+        );
+    }
 
-    this.eventService.getEventById(1).subscribe(
-        value => {
-          this.event = value;
-        },
-        e => console.log(e)
-    );
-  }
+    changeEventParticipation(): void {
 
-  getTypes(): void {
-    this.eventService.getEventType().subscribe(
-        val => {
-          val.forEach((type) => {
-                const typeToAdd: EventType = {
-                  id: type.id,
-                  name: type.name
-                };
-                this.types.push(typeToAdd);
-              }
-          );
-        }
-    );
-  }
+        this.eventService.changeEventParticipation(this.event.id).subscribe(
+            _ => // TODO : gestion erreur
+                e => console.log(e)
+        );
+        this.ngZone.run(() => this.getEvent())
+    }
 
-  segmentChanged(ev: any) {
+    getTypes(): void {
+        this.eventService.getEventType().subscribe(
+            val => {
+                val.forEach((type) => {
+                        const typeToAdd: EventType = {
+                            id: type.id,
+                            name: type.name
+                        };
+                        this.types.push(typeToAdd);
+                    }
+                );
+            }
+        );
+    }
 
-    this.selectedSegment = ev.detail.value;
-  }
+    segmentChanged(ev: any) {
+
+        this.selectedSegment = ev.detail.value;
+    }
 }
