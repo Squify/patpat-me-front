6214bb2282128d2748a178e21a802ac550ea2a39
Pauline <40692004+Squify@users.noreply.github.com>
2020-05-20 15:52:08 +0200

    correct and upgrade some things

--- src/app/pages/event/event.page.ts
+++ src/app/pages/event/event.page.ts
@@ -4,6 +4,9 @@ import {EventInterface} from '../../interfaces/event/event-interface';
 import {EventType} from '../../interfaces/event/event-type';
 import {UserService} from "../../services/user/user.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {HttpErrorResponse} from "@angular/common/http";
+import {ToastController} from "@ionic/angular";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
     selector: 'app-event',
@@ -16,6 +19,12 @@ export class EventPage implements OnInit {
     types: EventType[] = [];
     eventId: number = null;
 
+    // Errors
+    unknownError: boolean;
+    serverError: boolean;
+    inputsError: boolean;
+    userIsOwnerError: boolean;
+
     selectedSegment = 'informationSegment';
     type = 'Randonnée';
     url = '/src/assets/images/event-type/' + this.type + '.jpg';
@@ -27,6 +36,8 @@ export class EventPage implements OnInit {
         private userService: UserService,
         public router: Router,
         private activatedRoute: ActivatedRoute,
+        public toastController: ToastController,
+        public translate: TranslateService
     ) {
     }
 
@@ -54,7 +65,7 @@ export class EventPage implements OnInit {
     changeEventParticipation(): void {
 
         this.eventService.changeEventParticipation(this.event.id).subscribe(
-            _ => // TODO : gestion erreur
+            _ =>
                 e => console.log(e)
         );
         this.ngZone.run(() => this.getEvent())
@@ -87,4 +98,66 @@ export class EventPage implements OnInit {
         return this.event.owner.id == connectedUserId;
     }
 
+    processError(error: HttpErrorResponse) {
+        if (error) {
+            switch (error.status) {
+                case 400:
+                    this.inputsError = true;
+                    this.presentToast('back_input');
+                    break;
+                case 417:
+                    this.userIsOwnerError = true;
+                    this.presentToast('back_userIsOwner');
+                    break;
+                case 500:
+                    this.serverError = true;
+                    this.presentToast('back_server');
+                    break;
+                default:
+                    this.unknownError = true;
+                    this.presentToast('back_unknown');
+                    break;
+            }
+        } else {
+            this.unknownError = true;
+            this.presentToast('back_unknown');
+        }
+    }
+
+    async presentToast(error: string) {
+        let toast: HTMLIonToastElement;
+        switch (error) {
+            case 'back_input':
+                toast = await this.toastController.create({
+                    message: this.translate.instant('ERRORS.BACK_INPUT'),
+                    duration: 2000
+                });
+                break;
+            case 'back_userIsOwner':
+                toast = await this.toastController.create({
+                    message: this.translate.instant('ERRORS.USER_IS_OWNER'),
+                    duration: 2000
+                });
+                break;
+            case 'back_server':
+                toast = await this.toastController.create({
+                    message: this.translate.instant('ERRORS.BACK_SERVER'),
+                    duration: 2000
+                });
+                break;
+            case 'back_unknown':
+                toast = await this.toastController.create({
+                    message: this.translate.instant('ERRORS.BACK_UNKNOWN'),
+                    duration: 2000
+                });
+                break;
+            default:
+                toast = await this.toastController.create({
+                    message: this.translate.instant('ERRORS.DEFAULT'),
+                    duration: 3000
+                });
+                break;
+        }
+        toast.present();
+    }
 }
