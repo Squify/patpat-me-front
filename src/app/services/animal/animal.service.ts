ef4bb424281edde3676ae8fab1e77e4cd19abfe3
Pauline <gouleypauline@live.fr>
2020-05-27 14:52:01 +0200

    edit conflict

--- src/app/services/animal/animal.service.ts
+++ src/app/services/animal/animal.service.ts
@@ -1,12 +1,11 @@
-import {Injectable} from '@angular/core';
-import {Breed} from 'src/app/interfaces/animal/breed';
-import {Temper} from 'src/app/interfaces/animal/temper';
-import {HttpClient, HttpParams} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {environment} from 'src/environments/environment';
-import {CreateAnimal} from 'src/app/interfaces/animal/create-animal';
+import { Injectable } from '@angular/core';
+import { Breed } from 'src/app/interfaces/animal/breed';
+import { Temper } from 'src/app/interfaces/animal/temper';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { CreateAnimal } from 'src/app/interfaces/animal/create-animal';
 import { UpdateAnimal } from 'src/app/interfaces/animal/update-animal';
-import { AnimalInterface } from 'src/app/interfaces/animal/animal-interface';
 import { Animal } from "../../interfaces/animal/animal";
 
 @Injectable({
@@ -35,11 +34,9 @@ export class AnimalService {
         return this.http.get<Breed[]>(environment.BACKEND_URL + '/api/animal/breeds');
     }
 
-    //TODO recup info de l'animal dans le back à revoir 
     public getAnimalById(animalId: number): Observable<Animal> {
-
         const params: HttpParams = new HttpParams().set('animalId', animalId.toString());
-        return this.http.get<AnimalInterface>(environment.BACKEND_URL + '/api/animal', {params});
+        return this.http.get<Animal>(environment.BACKEND_URL + '/api/animal', {params});
     }
 
 }
