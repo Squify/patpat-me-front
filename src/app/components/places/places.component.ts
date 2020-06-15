9889cfc9b99a207d4db2d22aac2379bef1b0f4ab
Pauline <40692004+Squify@users.noreply.github.com>
2020-06-16 01:50:08 +0200

    add search bar to event edition page

--- src/app/components/places/places.component.ts
+++ src/app/components/places/places.component.ts
@@ -1,34 +1,39 @@
-import { AfterViewInit, Component, EventEmitter, OnDestroy, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, NgZone, OnDestroy, Output, ViewChild } from '@angular/core';
 import places from "places.js";
 import { GeolocationService } from "../../services/geolocation.service";
 
 @Component({
-  selector: "app-places",
-  templateUrl: './places.component.html',
-  styleUrls: ['./places.component.scss'],
+    selector: "app-places",
+    templateUrl: './places.component.html',
+    styleUrls: ['./places.component.scss'],
 })
 export class PlacesComponent implements AfterViewInit, OnDestroy {
-  private instance = null;
-  public data: string;
+    private instance = null;
+    public data: string;
 
-  @ViewChild("input", null) input;
-  @Output() onChange? = new EventEmitter();
+    @ViewChild("input", null) input;
+    @Output() onChange? = new EventEmitter();
+    @Input() editLocation: string;
 
-  constructor(private geolocationService: GeolocationService) {
-    this.geolocationService.myMethod(this.data);
-  }
+    constructor(
+        private geolocationService: GeolocationService,
+        private ngZone: NgZone,
+    ) {
+    }
 
-  ngAfterViewInit() {
-    this.instance = places({
-      container: this.input.nativeElement
-      // places options
-    });
-  }
-  ngOnDestroy() {
-    this.instance.destroy();
-  }
+    ngAfterViewInit() {
+        this.instance = places({
+            container: this.input.nativeElement
+            // places options
+        });
+        this.ngZone.run(() => this.input.nativeElement.value = this.editLocation);
+    }
 
-  sendValue() {
-    this.geolocationService.myMethod(this.input.nativeElement.value);
-  }
+    ngOnDestroy() {
+        this.instance.destroy();
+    }
+
+    sendValue() {
+        this.geolocationService.myMethod(this.input.nativeElement.value);
+    }
 }
