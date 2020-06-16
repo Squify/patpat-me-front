510a32a8effd3ca4f14004637f32882097c59078
Pauline <40692004+Squify@users.noreply.github.com>
2020-06-16 16:21:36 +0200

    correct dashboard filter function

--- src/app/pipes/event/event-filter.pipe.ts
+++ src/app/pipes/event/event-filter.pipe.ts
@@ -1,24 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'eventFilter'
+    name: 'eventFilter'
 })
 export class EventFilterPipe implements PipeTransform {
 
-  transform(events: any[], filter: any): any {
-    if (!events || !filter) {
-      return events;
+    transform(events: any[], filter: any): any {
+        if (!events || !filter) {
+            return events;
+        }
+        if (filter.length == 0) {
+            return events;
+        }
+        let result = [];
+        events.forEach(item => {
+            if (filter.includes(item.type.name)) {
+                result.push(item)
+            }
+        })
+        return result;
     }
-    if (filter.length == 0) {
-      return events;
-    }
-    let result = [];
-    events.forEach(item => {
-      if (filter.includes(item.type)) {
-        result.push(item)
-      }
-    })
-    return result;
-  }
 
 }
