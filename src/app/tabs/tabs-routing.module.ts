8f8d1a25e1117842ca42c1ad36a619a51920f0aa
Pauline <40692004+Squify@users.noreply.github.com>
2020-07-04 01:33:58 +0200

    change user profile route

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -45,7 +45,27 @@ const routes: Routes = [
                         path: '',
                         loadChildren: () =>
                             import('../pages/meet/meet.module').then(m => m.MeetPageModule)
-                    }
+                    },
+                    {
+                        path: 'user/:id',
+                        children: [
+                            {
+                                path: '',
+                                loadChildren: () =>
+                                    import('../pages/user-profile/user-profile.module').then(m => m.UserProfilePageModule)
+                            },
+                            {
+                                path: 'animal/:id',
+                                children: [
+                                    {
+                                        path: '',
+                                        loadChildren: () =>
+                                            import('../pages/animal/animal.module').then(m => m.AnimalPageModule)
+                                    },
+                                ]
+                            }
+                        ]
+                    },
                 ]
             },
             {
@@ -55,7 +75,27 @@ const routes: Routes = [
                         path: '',
                         loadChildren: () =>
                             import('../pages/friends/friends.module').then(m => m.FriendsPageModule)
-                    }
+                    },
+                    {
+                        path: 'user/:id',
+                        children: [
+                            {
+                                path: '',
+                                loadChildren: () =>
+                                    import('../pages/user-profile/user-profile.module').then(m => m.UserProfilePageModule)
+                            },
+                            {
+                                path: 'animal/:id',
+                                children: [
+                                    {
+                                        path: '',
+                                        loadChildren: () =>
+                                            import('../pages/animal/animal.module').then(m => m.AnimalPageModule)
+                                    },
+                                ]
+                            }
+                        ]
+                    },
                 ]
             },
             {
@@ -90,26 +130,26 @@ const routes: Routes = [
                     }
                 ]
             },
-            {
-                path: 'user/:id',
-                children: [
-                    {
-                        path: '',
-                        loadChildren: () =>
-                            import('../pages/user-profile/user-profile.module').then(m => m.UserProfilePageModule)
-                    },
-                    {
-                        path: 'animal/:id',
-                        children: [
-                            {
-                                path: '',
-                                loadChildren: () =>
-                                    import('../pages/animal/animal.module').then(m => m.AnimalPageModule)
-                            },
-                        ]
-                    }
-                ]
-            },
+            // {
+            //     path: 'user/:id',
+            //     children: [
+            //         {
+            //             path: '',
+            //             loadChildren: () =>
+            //                 import('../pages/user-profile/user-profile.module').then(m => m.UserProfilePageModule)
+            //         },
+            //         {
+            //             path: 'animal/:id',
+            //             children: [
+            //                 {
+            //                     path: '',
+            //                     loadChildren: () =>
+            //                         import('../pages/animal/animal.module').then(m => m.AnimalPageModule)
+            //                 },
+            //             ]
+            //         }
+            //     ]
+            // },
             {
                 path: '',
                 redirectTo: '/tabs/events',
