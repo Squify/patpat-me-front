ebe1fc9e4af4f01af4179273da5810750eb7d667
Pauline <40692004+Squify@users.noreply.github.com>
2020-07-26 16:25:39 +0200

    correct code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    BACKEND_URL: 'http://192.168.1.12:8000',
+    BACKEND_URL: 'http://api.patpatme.site:8000',
     default_profile_pic: '/assets/images/profile_pic/profile_default.png',
     default_cat_pic: '/assets/images/cat_pic/cat_1.png',
     default_dog_pic: '/assets/images/dog_pic/dog_6.png'
